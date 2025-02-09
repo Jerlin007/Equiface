4658990d84d9f7e4e4fc1f1abec7cd03cca972a4
Jerlin007 <jerlin.jzy7@gmail.com>
2025-02-09 11:58:13 +0530

    app.py

--- app.py
+++ app.py
@@ -160,7 +160,9 @@ def upload():
     return jsonify({"error": "Invalid file type"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
